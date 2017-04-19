42f647caa34f4c3df1aea29c8a4b1aedc71b387b
Serena Stater <srstater@gmail.com>
2017-04-19 17:12:56 -0400

    some progress on largestSmallest

--- largestSmallest.py
+++ largestSmallest.py
@@ -5,3 +5,32 @@
 # try/except and put out an appropriate message and ignore the
 # number. Enter the numbers from the book for problem 5.1 and
 # Match the desired output as shown.
+
+# Enter a number: 4
+# Enter a number: 5
+# Enter a number: bad data
+# Invalid input
+# Enter a number: 7
+# Enter a number: done
+# 16 3 5.33333333333
+
+largest = None
+smallest = None
+
+try:
+    while True:
+        inp = raw_input("Enter a number: ")
+        num = float(inp)
+        if num == "done":
+            break
+        elif num < smallest:
+            smallest = num
+        elif num > largest:
+            largest = num
+        print num, smallest, largest
+except:
+    if type(num) != int:
+        print("Invalid input")
+
+
+#print "Maximum", largest
